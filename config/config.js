6d29133834f774e012e38627eeab36aa7c752825
Song <richieqin@gmail.com>
2015-07-03 18:05:16 -0400

    better

--- config/config.js
+++ config/config.js
@@ -2,4 +2,4 @@
 
 //load current configuration file accoring to the 'NODE_ENV' variable
 
-module.exports = require('./env/' + process.env.NODE_ENV + '.js');
+module.exports = require('env/' + process.env.NODE_ENV + '.js');
