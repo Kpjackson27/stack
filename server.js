789bb1973d6d4525af57644294b4b8bf07843d3b
Song <richieqin@gmail.com>
2015-07-03 18:00:54 -0400

    better

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 'use strict';
 
 //Set the 'NODE_ENV' variable
-process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+// process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 var mongoose = require('./config/mongoose'),
 	express = require('./config/express'),
@@ -17,10 +17,13 @@ var app = express(db);
 var passport = require('passport');
 
 //use express application instance to listen to '3000' port
-app.listen(3000);
-
+// app.listen(3000);
+app.listen(process.env.PORT || 3000, function(){
+  console.log('listening on', app.address().port);
+});
+console('process.env.PORT' +process.env.PORT);
 //log server status to the console
-console.log('Server running at http://127.0.0.1:3000/');
+// console.log('Server running at http://127.0.0.1:3000/');
 
 //Use module.exports property to expose express application instance for external usage
 module.exports = app;
