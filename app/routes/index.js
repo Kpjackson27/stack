abe4f4386081acc36320ec6716d213c222973cb6
Kareem <kpjackson27@hotmail.com>
2015-07-06 19:16:03 -0400

    modal

--- app/routes/index.js
+++ app/routes/index.js
@@ -4,6 +4,8 @@
 var users = require('../../app/controllers/users'),
 	passport = require('passport'),
 	index = require('../controllers/index'),
+	articles = require('../../app/controllers/articles'),
+	favorites = require('../../app/controllers/favorites'),
 	passportConf = require('../../config/passport');
 
 module.exports = function(app){
@@ -11,4 +13,5 @@ module.exports = function(app){
 	//Mount the 'index' controller's 'render' method
 	app.route('/')
 		.get(passportConf.isAuthenticated,index.render);
+	   	//.get(index.list);
 };
