981271e5b330665433a92a79542379f00669757f
Kareem <kpjackson27@hotmail.com>
2015-07-06 20:07:26 -0400

    profile

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,58 +1,8 @@
 'use strict';
 
-// Load the module dependencies
-var mongoose = require('mongoose'),
-	Article = mongoose.model('Article');
-
-
-// Create a new error handling controller method
-var getErrorMessage = function(err) {
-	if (err.errors) {
-		for (var errName in err.errors) {
-			if (err.errors[errName].message) return err.errors[errName].message;
-		}
-	} else {
-		return 'Unknown server error';
-	}
-};
-
 //Create a new 'render' controller method
 exports.render = function(req,res){
 	res.render('index', {
 		title: 'Lyrical Intent',
-	});
-};
-
-
-// return a list of articles
-exports.list = function(req, res) {
-	// var page = (req.param('page') > 0 ? req.param('page'):1) - 1;
-	// var perPage = 15;
-	// var options = {
-	// perPage: perPage,
-	// page: page
-	// };
-
-	// Use the model 'find' method to get a list of articles
-	Article.find().sort('-created').populate('creator', 'email profile profile.name').exec(function(err, articles) {
-		if (err) {
-			req.flash('errors', {
-				msg: getErrorMessage(err)
-			});
-			return res.redirect('/');
-		} else {
-			console.log(articles);
-			res.format({
-				html: function() {
-					res.render('/', {
-						title: 'All Poems',
-						"articles": articles
-					});
-				},
-				json: function() {
-					res.json(articles);
-				}
-			});
-		}
 	});
 };
--- app/routes/index.js
+++ app/routes/index.js
@@ -4,8 +4,6 @@
 var users = require('../../app/controllers/users'),
 	passport = require('passport'),
 	index = require('../controllers/index'),
-	articles = require('../../app/controllers/articles'),
-	favorites = require('../../app/controllers/favorites'),
 	passportConf = require('../../config/passport');
 
 module.exports = function(app){
@@ -13,5 +11,5 @@ module.exports = function(app){
 	//Mount the 'index' controller's 'render' method
 	app.route('/')
 		.get(passportConf.isAuthenticated,index.render);
-	   	//.get(index.list);
+	   
 };
