4dd05d56f1631d75012c81531994f5b1acfdd2fb
Kareem <kpjackson27@hotmail.com>
2015-07-12 21:02:47 -0400

    test

--- config/env/production.js
+++ config/env/production.js
@@ -3,6 +3,6 @@
 //set the 'production' environment configuration object
 module.exports = {
 	// db: 'mongodb://localhost/lyrical-intent',
-	db: 'mongodb://test:test@ds041150.mongolab.com:41150/heroku_dpk4p3x7',
+	db: 'mongodb://song:123456@ds041150.mongolab.com:41150/verz',
 	sessionSecret: 'developmentSessionSecret'
 };
