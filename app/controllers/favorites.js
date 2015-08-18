762bbb5f29da819d28366f5b0f57e31446974acc
Song <richieqin@gmail.com>
2015-08-18 14:06:56 -0400

    raccoon

--- app/controllers/favorites.js
+++ app/controllers/favorites.js
@@ -1,4 +1,3 @@
-
 'use strict';
 
 // var _ = require('lodash'),
@@ -10,31 +9,41 @@
 //   Article = require('../models/Article');
 
 //import raccoon module
+var raccoon = require('raccoon');
+//import redies module
+exports.create = function(req, res) {
+    // var article = req.article;
+    // // console.log(req.article);
+    // article._favorites = req.user;
+    // article.save(function (err) {
+    //   if (err) {
+    //     req.flash('errors', { msg: 'Oops! Failed to like verse.'});
+    //   } 
+    //   res.redirect('/api/articles');
 
-exports.create = function (req, res) {
-  var article = req.article;
-  // console.log(req.article);
-  article._favorites = req.user;
-  article.save(function (err) {
-    if (err) {
-      req.flash('errors', { msg: 'Oops! Failed to like verse.'});
-    } 
-    res.redirect('/api/articles');
+    // });
+    //recommendation data
+    //article id
+    console.log(req.article._id);
+    //user id
+    console.log(req.user._id);
+    //liked function to store a like instance in redis server
+    // raccoon.liked(req.article._id, req.user._id, function() {
+    //     client.smembers('movie:chris:liked', function(err, results) {
+    //         console.log(results[0], 'batman');
+    //         res.redirect('/api/articles');
+    //     });
+    // });
 
-  });
-  //recommendation data
-  //article id
-  //user id
-  //liked function to store a like instance in redis server
 };
 
 
 // ### Delete Favorite
-exports.destroy = function (req, res) {
-  var article = req.article;
-  article._favorites = req.user;
-  article.save(function (err) {
-    if (err) return res.send(400);
-    res.send(200);
-  });
+exports.destroy = function(req, res) {
+    var article = req.article;
+    article._favorites = req.user;
+    article.save(function(err) {
+        if (err) return res.send(400);
+        res.send(200);
+    });
 };
